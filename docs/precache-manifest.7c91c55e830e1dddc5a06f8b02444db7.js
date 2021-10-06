4cac8fb64a0633869d8fa6ec8d9662bae4300606
legendof17 <legendof17gta@gmail.com>
2021-10-06 10:19:29 +0530

    docs

---
+++ docs/precache-manifest.7c91c55e830e1dddc5a06f8b02444db7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7b7aaad43d096af7e89138e8de057c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45d4c05401164ddde424",
+    "url": "/static/css/main.e0f77b50.chunk.css"
+  },
+  {
+    "revision": "cb6b04105a5bba37ad0b",
+    "url": "/static/js/2.8780fa47.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.8780fa47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45d4c05401164ddde424",
+    "url": "/static/js/main.4e46ca28.chunk.js"
+  },
+  {
+    "revision": "280e1a89c119e595585a",
+    "url": "/static/js/runtime-main.225e93f6.js"
+  }
+]);
